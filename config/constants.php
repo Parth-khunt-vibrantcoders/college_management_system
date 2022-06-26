7c6756e3f1a7d83f597499512bcd8af7280c2ac0
Parth Khunt <pkhunt@vibrantcodes.com>
2022-06-26 16:45:54 +0530

    cms setting

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+function ccd($data){
+    echo "<pre>";
+    print_r($data);
+    die();
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+
+include('config/constants.php');
+include('config/db_conn.php');
+?>
